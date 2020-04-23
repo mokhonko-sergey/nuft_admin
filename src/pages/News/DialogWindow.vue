864998e5785a9b237a59490e33572e9dcb455074
sergey mokhonko <sergiy35@gmail.com>
2020-04-23 20:11:36 +0300

    Feat: News. Update record

--- src/pages/News/DialogWindow.vue
+++ src/pages/News/DialogWindow.vue
@@ -1,13 +1,21 @@
 <template>
   <dialog-window :showDialog="isActive" :title="title">
     <template slot="content">
-      <h1>Dialog</h1>
+      <md-field>
+        <label>Title</label>
+        <md-input v-model="value.title"></md-input>
+      </md-field>
+      <md-field>
+        <label>Textarea</label>
+        <md-textarea v-model="value.content" md-counter="80"></md-textarea>
+      </md-field>
+      <md-switch v-model="value.visible" class="md-primary">Visible</md-switch>
     </template>
     <template slot="actions">
       <md-button @click="$emit('close-dialog')">
         Close
       </md-button>
-      <md-button class="md-success">
+      <md-button class="md-success" @click="action">
         Save
       </md-button>
     </template>
@@ -19,7 +27,33 @@ import DialogWindow from "@/components/Dialog";
 export default {
   props: {
     isActive: Boolean,
-    title: String
+    title: String,
+    action: {
+      type: Function,
+      required: true
+    },
+    value: {
+      type: Object,
+      default() {
+        return {
+          title: "",
+          content: "",
+          visible: ""
+        };
+      },
+      loading: {
+        type: Boolean,
+        default: false
+      }
+    }
+  },
+  watch: {
+    value: {
+      deep: true,
+      handler(value) {
+        this.$emit("input", value);
+      }
+    }
   },
   components: {
     DialogWindow
